c5112c2ff07af7ba5c7bafd753a10bb855fbed83
XYZ <amitbkk1984@gmail.com>
2019-05-23 16:05:59 +0700

    Added migrations and attributes

--- EFCodeFirstDemo/Program.cs
+++ EFCodeFirstDemo/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace EFCodeFirstDemo
 {
@@ -12,47 +13,108 @@ namespace EFCodeFirstDemo
     {
         public class Student
         {
-            
+
             public int StudentId { get; set; }
             public string LastName { get; set; }
             public string FirstMidName { get; set; }
-            public DateTime  EnrollmentDate { get; set; }
+           public int Age { get; set; }
+            public DateTime EnrollmentDate { get; set; }
+            public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        }
+        public class StudentLogIn
+        {
+            [Key, ForeignKey("Student")]
+            public int ID { get; set; }
+            public string EmailID { get; set; }
+            public string Password { get; set; }
+
+            public virtual Student Student { get; set; }
+        }
+        public class Course
+        {
+            public int CourseId { get; set; }
+           [MaxLength(25),MinLength(4)]
+            public string Title { get; set; }
+            public int Credit { get; set; }
+            [Timestamp ]
+           public byte[] RowVersion { get; set; }
             public virtual ICollection<Enrollment> Enrollments { get; set; }
 
         }
 
-        public class Course
-        {
-            public int CourseId { get; set; }
-            public string Title { get; set; }
-            public int Credit { get; set; }
-            public virtual ICollection< Enrollment > Enrollments { get; set; }
-           
-        }
-
         public enum Grade
-        { A,B,C,D,F}
+        { A, B, C, D, F }
         public class Enrollment
         {
+
             public int EnrollmentID { get; set; }
             public int CourseID { get; set; }
             public int StudentID { get; set; }
-            public Grade ? Grades { get; set; }
+            public Grade? Grades { get; set; }
+           
+            public virtual Course Course { get; set; }
             
+            public virtual Student Student { get; set; }
+
         }
 
-        public class Mycontext : DbContext
+        public class MyContext : DbContext
         {
-            public Mycontext() : base() { }
-            public virtual DbSet<Student> Students { get; set; }
-            public virtual DbSet<Course > Courses { get; set; }
+
+            public MyContext() : base("name=StudentEnrollmentDB")
+            {
+                 Database.SetInitializer<MyContext>(new UniDBInitializer<MyContext>());
+                //    EFCodeFirstDemo.Migrations.Configuration > (MyContext);
+                //}
+                 Database.SetInitializer(new MigrateDatabaseToLatestVersion<MyContext, EFCodeFirstDemo.Migrations.Configuration>("StudentEnrollmentDB"));
+            }
+
+            public virtual DbSet<Course> Courses { get; set; }
             public virtual DbSet<Enrollment> Enrollments { get; set; }
+            public virtual DbSet<Student> Students { get; set; }
+            public virtual DbSet<StudentLogIn> StudentsLogIn { get; set; }
+            private class UniDBInitializer<T> : DropCreateDatabaseAlways<MyContext>
+            {
+
+                protected override void Seed(MyContext context)
+                {
+
+                    IList<Student> students = new List<Student>();
+
+                    students.Add(new Student()
+                    {
+                        FirstMidName = "Andrew",
+                        LastName = "Peters",
+                        EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                    });
+
+                    students.Add(new Student()
+                    {
+                        FirstMidName = "Brice",
+                        LastName = "Lambson",
+                        EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                    });
+
+                    students.Add(new Student()
+                    {
+                        FirstMidName = "Rowan",
+                        LastName = "Miller",
+                        EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                    });
+
+                    foreach (Student student in students)
+                        context.Students.Add(student);
+                    context.SaveChanges();
+                    base.Seed(context);
+                }
+            }
         }
 
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            using (var Context = new Mycontext())
+            using (var Context = new MyContext())
             {
                 Console.WriteLine("Adding new Student");
                 Console.WriteLine("==========================================================");
@@ -74,10 +136,11 @@ namespace EFCodeFirstDemo
                 {
                     Console.WriteLine("Student LastName=" + std.LastName + " " + "firstname =" + std.FirstMidName + " " + "Enrollment date=" + std.EnrollmentDate); ;
                 }
-                Console.WriteLine( "retreving All student complete");
+                Console.WriteLine("retreving All student complete");
                 Console.WriteLine("==============================================================");
 
             }
         }
     }
 }
+
